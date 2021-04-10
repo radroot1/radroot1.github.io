e84017a3633e79807e648d252643c0870fc5758a
radroot1 <radroot1@gmail.com>
2021-04-10 18:46:07 +0400

    web link paste

--- scripts/pasterScript.js
+++ scripts/pasterScript.js
@@ -1,5 +1,6 @@
 window.onload = async function () {
     let mCount = 0;
+    let mWeb = false;
     const pContainer = document.getElementById("elements");
     const pCounter = document.getElementById("counter");
     function setCount(){
@@ -8,36 +9,46 @@ window.onload = async function () {
         pCounter.innerHTML = count.toLocaleString() + ' / ' + charLength.toLocaleString();
         mCount = count;
     }
-    function doListen(b){
+    function doListenForClipChanges(b){
         if(b){
-            pContainer.addEventListener("DOMNodeInserted", listener, false);
+            pContainer.addEventListener("DOMNodeInserted", clipListener, false);
         } else{
-            pContainer.removeEventListener("DOMNodeInserted", listener, false);
+            pContainer.removeEventListener("DOMNodeInserted", clipListener, false);
         }
     }
-    function doWithoutListen(callback){
-        doListen(false);
+    function modifyWithoutClipListen(callback){
+        doListenForClipChanges(false);
         setTimeout(callback, 0);
-        doListen(true);
+        doListenForClipChanges(true);
     }
-    const listener = function () {
-        const elements = pContainer.getElementsByTagName('p');
-        const countNew = elements.length;
-        if (countNew - mCount > 0) {
-            const newEl = elements[countNew - 1];
-            const newline = newEl.innerHTML;
-            if (newline.indexOf('. ') === -1) {
-                newEl.innerHTML = newline + '. '
-            }
-            if(countNew > 1){
-                doWithoutListen(()=>{
+    function clipListener() {
+        modifyWithoutClipListen(() => {
+            const elements = pContainer.getElementsByTagName('p');
+            const countNew = elements.length;
+            if (countNew - mCount > 0) {
+                const newEl = elements[countNew - 1];
+                const newElContent = newEl.innerHTML;
+                if(mWeb){
+                    var a = document.createElement('a');
+                    var linkText = document.createTextNode(newElContent);
+                    a.appendChild(linkText);
+                    a.title = newElContent;
+                    a.href = newElContent;
+                    newEl.innerHTML = "";
+                    newEl.appendChild(a);
+                } else {
+                    if (newElContent.indexOf('. ') === -1) {
+                        newEl.innerHTML = newElContent + '. ';
+                    }
+                }
+                if (countNew > 1) {
                     pContainer.insertBefore(newEl, elements[0]);
-                });
+                }
+                setCount();
             }
-            setCount();
-        }
-    };
-    doListen(true);
+        })
+    }
+    doListenForClipChanges(true);
     document.getElementById("remove_button").addEventListener("click", function () {
         const elements = pContainer.getElementsByTagName('p');
         if (elements.length > 0) {
@@ -46,6 +57,10 @@ window.onload = async function () {
             pContainer.insertBefore(first, null);
             setCount();
         }
+    });
+    document.getElementById("web_button").addEventListener("click", function () {
+        mWeb = true
+
     });
     const text = await navigator.clipboard.readText();
     pContainer.innerHTML = `<p>${text}</p>`;
